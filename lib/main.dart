1ca3ca687782416c67e692270b6e31f2cb5ff07c
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-20 01:44:42 +0700

    add hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
   final settingsController = SettingsController(SettingsService());
-
   // await settingsController.loadSettings();
-
   runApp(App(settingsController: settingsController));
 }
