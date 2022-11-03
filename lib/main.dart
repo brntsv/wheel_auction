df8480be4f97ba1a07f0e6ffa2ae0e4414aebfca
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-03 22:42:53 +0700

    delete useless hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
 
 import 'src/app.dart';
-import 'src/models/list_of_wheels_model.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
 
   final settingsController = SettingsController(SettingsService());
   // await settingsController.loadSettings();
   runApp(
-    App(settingsController: settingsController),
+    MyApp(settingsController: settingsController),
   );
 }
