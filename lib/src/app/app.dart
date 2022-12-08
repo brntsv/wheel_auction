17fa0f2b7f38ef24f30beb59f62b35e8156e7edd
Nikita Barantsev <nbarentsev@gmail.com>
2022-12-09 01:31:01 +0700

    folder managment for app

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:wheel_auction/src/providers/list_of_wheels_provider.dart';
+import 'package:wheel_auction/src/screens/add_wheel_screen/add_wheel_screen.dart';
+import 'package:wheel_auction/src/screens/global_settings_screen/global_settings_screen.dart';
+import 'package:wheel_auction/src/screens/main_screen/main_screen.dart';
+import 'package:wheel_auction/src/screens/settings_of_chosen_screen/settings_of_chosen_wheel.dart';
+import 'package:wheel_auction/src/settings/settings_controller.dart';
+import 'package:wheel_auction/src/theme/app_theme.dart';
 
-import 'models/list_of_wheels_model.dart';
-import 'screens/add_wheel_screen.dart';
-import 'screens/global_settings_screen.dart';
-import 'screens/main_screen_wheel.dart';
-import 'screens/settings_of_chosen_wheel.dart';
-import 'settings/settings_controller.dart';
-import 'theme/app_theme.dart';
-
-class App extends StatelessWidget {
-  const App({
+class MyApp extends StatelessWidget {
+  const MyApp({
     Key? key,
     required this.settingsController,
   }) : super(key: key);
@@ -29,37 +26,24 @@ class App extends StatelessWidget {
             ChangeNotifierProvider(
               create: (context) => ThemeProvider(),
               builder: (context, _) {
-                final themeProvider = Provider.of<ThemeProvider>(context);
+                final themeProvider = context.watch<ThemeProvider>();
+
                 return MaterialApp(
-                  restorationScopeId: 'app',
-                  localizationsDelegates: const [
-                    AppLocalizations.delegate,
-                    GlobalMaterialLocalizations.delegate,
-                    GlobalWidgetsLocalizations.delegate,
-                    GlobalCupertinoLocalizations.delegate,
-                  ],
-                  supportedLocales: const [
-                    Locale('en', ''),
-                  ],
-                  onGenerateTitle: (BuildContext context) =>
-                      AppLocalizations.of(context)!.appTitle,
                   debugShowCheckedModeBanner: false,
                   //
                   theme: MyThemes.lightTheme,
                   darkTheme: MyThemes.darkTheme,
                   themeMode: themeProvider.selectedThemeMode,
                   //
-                  home: child,
                   onGenerateRoute: (RouteSettings routeSettings) {
                     return MaterialPageRoute<void>(
                       settings: routeSettings,
                       builder: (BuildContext context) {
                         switch (routeSettings.name) {
                           case AddWheelScreen.routeName:
-                            return AddWheelScreen(
-                                controller: settingsController);
+                            return const AddWheelScreen();
                           case GlobalSettingsWheel.routeName:
-                            return GlobalSettingsWheel();
+                            return const GlobalSettingsWheel();
                           case SettingsOfChosenWheel.routeName:
                             return const SettingsOfChosenWheel();
                           case MainScreen.routeName:
@@ -71,10 +55,14 @@ class App extends StatelessWidget {
                   },
                 );
               },
+              child: ChangeNotifierProvider<ListOfWheelsProviderPart2>(
+                create: (context) => ListOfWheelsProviderPart2(),
+              ),
             ),
-            ChangeNotifierProvider(create: (_) => ListOfWheelsModel()),
+            ChangeNotifierProvider<ListOfWheelsProvider>(
+              create: (context) => ListOfWheelsProvider(),
+            ),
           ],
-          child: child,
         );
       },
     );
