2aa15bdf5712926a4d7e81f9b076b91b9ae810c3
Nikita Barantsev <nbarentsev@gmail.com>
2022-12-13 14:04:58 +0700

    make simple MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wheel_auction/src/app/app.dart';
-import 'package:wheel_auction/src/settings/settings_controller.dart';
 
 void main() async {
-  final settingsController = SettingsController();
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const MyApp());
 }
--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -6,60 +6,49 @@ import 'package:wheel_auction/src/screens/main_screen/main_screen.dart';
 import 'package:wheel_auction/src/screens/main_screen/model/list_of_wheels_model.dart';
 import 'package:wheel_auction/src/screens/main_screen/widgets/change_theme_button/theme_provider.dart';
 import 'package:wheel_auction/src/screens/settings_of_chosen_screen/settings_of_chosen_wheel.dart';
-import 'package:wheel_auction/src/settings/settings_controller.dart';
 import 'package:wheel_auction/src/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    Key? key,
-    required this.settingsController,
-  }) : super(key: key);
-
-  final SettingsController settingsController;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MultiProvider(
-          providers: [
-            ChangeNotifierProvider<ListOfWheelsModel>(
-              create: (context) => ListOfWheelsModel(),
-            ),
-            ChangeNotifierProvider(
-              create: (context) => ThemeProvider(),
-              builder: (context, _) {
-                final themeProvider = context.watch<ThemeProvider>();
-                return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  theme: MyThemes.lightTheme,
-                  darkTheme: MyThemes.darkTheme,
-                  themeMode: themeProvider.selectedThemeMode,
-                  onGenerateRoute: (RouteSettings routeSettings) {
-                    return MaterialPageRoute<void>(
-                      settings: routeSettings,
-                      builder: (BuildContext context) {
-                        switch (routeSettings.name) {
-                          case AddWheelScreen.routeName:
-                            return const AddWheelScreen();
-                          case GlobalSettingsWheel.routeName:
-                            return const GlobalSettingsWheel();
-                          case SettingsOfChosenWheel.routeName:
-                            return const SettingsOfChosenWheel();
-                          case MainScreen.routeName:
-                          default:
-                            return const MainScreen();
-                        }
-                      },
-                    );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ListOfWheelsModel>(
+          create: (context) => ListOfWheelsModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+          builder: (context, _) {
+            final themeProvider = context.watch<ThemeProvider>();
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: MyThemes.lightTheme,
+              darkTheme: MyThemes.darkTheme,
+              themeMode: themeProvider.selectedThemeMode,
+              onGenerateRoute: (RouteSettings routeSettings) {
+                return MaterialPageRoute<void>(
+                  settings: routeSettings,
+                  builder: (BuildContext context) {
+                    switch (routeSettings.name) {
+                      case AddWheelScreen.routeName:
+                        return const AddWheelScreen();
+                      case GlobalSettingsWheel.routeName:
+                        return const GlobalSettingsWheel();
+                      case SettingsOfChosenWheel.routeName:
+                        return const SettingsOfChosenWheel();
+                      case MainScreen.routeName:
+                      default:
+                        return const MainScreen();
+                    }
                   },
                 );
               },
-            ),
-          ],
-        );
-      },
+            );
+          },
+        ),
+      ],
     );
   }
 }
