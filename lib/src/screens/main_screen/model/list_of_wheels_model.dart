e86ce5d6323f9c06d97061cf858ba9a84e00d479
Nikita Barantsev <nbarentsev@gmail.com>
2022-12-15 01:54:12 +0700

    remake model

--- lib/src/screens/main_screen/model/list_of_wheels_model.dart
+++ lib/src/screens/main_screen/model/list_of_wheels_model.dart
@@ -1,30 +1,56 @@
 import 'package:flutter/material.dart';
 
-class WheelObject {
-  final String? nameOfWheel;
-  final List<String> eventOptions;
-  WheelObject({
-    required this.nameOfWheel,
-    required this.eventOptions,
-  });
-}
+// class WheelObject {
+//   final String? nameOfWheel;
+//   final List<String> eventOptions;
+//   WheelObject({
+//     required this.nameOfWheel,
+//     required this.eventOptions,
+//   });
+// }
+
+// class ListOfWheelsModel with ChangeNotifier {
+//   /// Внутреннее приватное состояние колес.
+//   final List<WheelObject> _items = [];
+
+//   /// Неизменяемое представление колес.
+//   List<WheelObject> get wheels => _items;
+
+//   /// метод добавления колеса в список колес
+//   void addWheel(WheelObject item) {
+//     _items.add(item);
+//     notifyListeners();
+//   }
+
+//   /// метод удаления колеса
+//   void removeWheel(WheelObject item) {
+//     _items.remove(item);
+//     notifyListeners();
+//   }
+// }
 
 class ListOfWheelsModel with ChangeNotifier {
   /// Внутреннее приватное состояние колес.
-  final List<WheelObject> _items = [];
+  final List<String?> _items = [];
 
-  /// Неизменяемое представление колес.
-  List<WheelObject> get wheels => _items;
+  /// представление названий колес.
+  List<String?> get wheels => _items;
+
+  /// Внутреннее приватное состояние событий.
+  final List<List<String?>> _listEvents = [];
+
+  /// представление событий колес.
+  List<List<String?>> get events => _listEvents;
 
   /// метод добавления колеса в список колес
-  void addWheel(WheelObject item) {
-    _items.add(item);
+  void addNameWheel(String? nameOfWheel) {
+    _items.add(nameOfWheel);
     notifyListeners();
   }
 
-  /// метод удаления колеса
-  void removeWheel(WheelObject item) {
-    _items.remove(item);
+  /// метод добавления событий колеса в список событий
+  void addListOfEvents(List<String> listOfEvents) {
+    _listEvents.add(listOfEvents);
     notifyListeners();
   }
 }
