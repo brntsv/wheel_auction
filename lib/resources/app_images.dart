0ff81d5981e337ca5a92ea48a8ce8fa818b2c626
Nikita Barantsev <nbarentsev@gmail.com>
2022-12-21 13:22:37 +0700

    add gifs for wheel

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,19 @@
+class AppImages {
+  AppImages._();
+  static const String anime = 'assets/images/anime.webp';
+  static const String blackCat = 'assets/images/black_cat.webp';
+  static const String clap = 'assets/images/clap.gif';
+  static const String duck = 'assets/images/duck.webp';
+  static const String jillette = 'assets/images/jillette.webp';
+  static const String papich = 'assets/images/papich.webp';
+  static const String pepegaBusiness = 'assets/images/pepega_business.webp';
+  static const String pepegaCoolman = 'assets/images/pepega_coolman.gif';
+  static const String pepegaEyes = 'assets/images/pepega_eyes.webp';
+  static const String pepegaSad = 'assets/images/pepega_sad.webp';
+  static const String punch = 'assets/images/punch.webp';
+  static const String wheelpog = 'assets/images/wheelpog.webp';
+  static const String whiteCat = 'assets/images/white_cat.webp';
+  static const String yellowboyAngry = 'assets/images/yellowboy_angry.webp';
+  static const String yellowboyCringe = 'assets/images/yellowboy_cringe.webp';
+  static const String yellowboyNamaz = 'assets/images/yellowboy_namaz.webp';
+}
