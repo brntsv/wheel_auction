239fbabe18d2e8a05f4473255c01c50e171219b6
Nikita Barantsev <nbarentsev@gmail.com>
2022-12-25 00:24:40 +0700

    stay orientation screen in portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wheel_auction/src/app/app.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  // оставляет ориентацию экрана всегда в одном положении
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
 }
